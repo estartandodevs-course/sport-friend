4e78a0219f0d7491622dba9fb372b3d586f9d0b2
Jaja <jarilson.rk@gmail.com>
2019-11-18 22:23:33 -0300

    data of user on app

---
+++ src/data/users.js
@@ -0,0 +1,36 @@
+export const users = [
+    {
+        "id": 1,
+        "first_name": "Angelo",
+        "last_name": "Lima",
+        // "avatar":
+        "age": 18,
+        "phone_number": 121212,
+        "bio": "lorem lorem lorem lorem lorem",
+        "favorite_sports": "Caminhada",
+        "place": {
+            "city": "Rio de Janeiro",
+            "coordinates": {
+                "latitude": 22.9749107,
+                "longitude": -43.6736966
+            }
+        }
+    },
+    {
+        "id": 2,
+        "first_name": "Breno",
+        "last_name": "Nunes",
+        // "avatar":
+        "age": 18,
+        "phone_number": 131313,
+        "bio": "lorem lorem lorem lorem lorem",
+        "favorite_sports": "Futebol",
+        "place": {
+            "city": "Rio de Janeiro",
+            "coordinates": {
+                "latitude": 22.9749107,
+                "longitude": -43.6736966
+            }
+        },
+    },
+]

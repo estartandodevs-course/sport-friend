813a9124c8ceaa0a8c9516c60f23fe78697b9607
Breno Nunes <brenontavares@gmail.com>
2019-11-20 02:39:34 -0300

    data of activities
    creating activities & updating users

---
+++ src/data/activities.js
@@ -0,0 +1,46 @@
+const activities = [
+    {
+        type: "Caminha",
+        // id of who created
+        author_id: 1,
+        description: "Leve caminhada",
+        date: {
+            day: 05,
+            month: 12,
+            year: 2019
+        },
+        moment: {
+            hour: "10:00",
+            part_day: "AM"
+        },
+        place: {
+            city: "Rio de Janeiro",
+            coordinates: {
+                latitude: 22.9749107,
+                longitude: -43.6736966
+            }
+        }
+    },
+    {
+        type: "Futebol",
+        // id of who created
+        author_id: 2,
+        description: "Futebol com alguns amigos, precisamos de mais pessoas!",
+        date: {
+            day: 15,
+            month: 12,
+            year: 2019
+        },
+        moment: {
+            hour: "05:00",
+            part_day: "PM"
+        },
+        place: {
+            city: "Rio de Janeiro",
+            coordinates: {
+                latitude: 22.9749107,
+                longitude: -43.6736966
+            }
+        }
+    }
+]

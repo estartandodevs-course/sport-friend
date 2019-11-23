62369ba00612b2f558f7371f3174b9a1f120659a
igor pereira <igorpereirapoliceno@gmail.com>
2019-11-23 12:21:15 -0300

    CardComponents --WIP

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header/header.js'
+import Card from '../../components/Card/card'
 import './home.scss';
 
 export default function Home() {
   return (
     <div>
         <Header />
+        <Card />
     </div>
   );
 }

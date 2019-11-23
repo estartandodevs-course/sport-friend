22b80438efe3924f7562d572639719cf85aea079
Jaja <jarilson.rk@gmail.com>
2019-11-23 20:16:56 -0300

    WIP

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './header.scss';
 import Menu from '../Menu/menu'
-import detalis from '../../assets/img/menu.svg'
 
 export default function Header(props) {
 
@@ -11,8 +10,8 @@ export default function Header(props) {
         setMenu(!menu)
     }
     return (
-        <div>
-            <img onClick={toogleMenu} src={detalis} className="menu" alt="" />
+        <div className="box">
+            <i onClick={toogleMenu} class="material-icons detalis">menu</i>
             <Menu close={toogleMenu} className="openMenu" openMenu={props.openMenu} left={menu ? "0" : "-320px"} />
         </div>
     );
--- src/components/Menu/menu.js
+++ src/components/Menu/menu.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import './menu.scss'
-
+import Logout from '../../assets/img/logout.png'
 export default function menu(props) {
     return (
-        <div className="Menu"
-        style={{ left: props.left }}>
-            <i onClick={props.close} class="material-icons ">close</i>
-        </div>
+        <section>
+            <div className="Menu" style={{ left: props.left }}>
+                <i onClick={props.close} class="material-icons close">close</i>
+                <div className="containerUser">
+                    <div class="user">
+                        <i class="material-icons">perm_identity</i>
+                    </div>
+                    <div>
+                        <h1>Angelo</h1>
+                    </div>
+                    <div className="user-perfil">
+                        <h2>Perfil</h2>
+                        <h2>Minhas atividades</h2>
+                        <h2>Historico</h2>
+                        <h2>Comentarios</h2>
+                        <div className="logout">
+                            <img src={Logout} alt="logout"></img>
+                            <h2>sair</h2>
+                        </div>
+                    </div>
+                    {/* <Link to="../Login/login"></Link> */}
+                </div>
+            </div>
+        </section>
     )
 }

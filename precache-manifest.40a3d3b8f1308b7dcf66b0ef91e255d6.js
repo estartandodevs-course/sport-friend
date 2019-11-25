3805ed41f891daef5ecf4f775ce98f9dacc60f8e
Alan Araujo <alan.techsafe@hotmail.com>
2019-11-25 02:38:47 -0300

    Updates

---
+++ precache-manifest.40a3d3b8f1308b7dcf66b0ef91e255d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbbc31889ab89b826f4a812416720f8",
+    "url": "/sport-friend/index.html"
+  },
+  {
+    "revision": "2c5beabc3466e14eb984",
+    "url": "/sport-friend/static/css/main.26bbc251.chunk.css"
+  },
+  {
+    "revision": "598fffc5f98fbd715611",
+    "url": "/sport-friend/static/js/2.a725dbbb.chunk.js"
+  },
+  {
+    "revision": "2c5beabc3466e14eb984",
+    "url": "/sport-friend/static/js/main.584d1d72.chunk.js"
+  },
+  {
+    "revision": "20da5df0fbc3a6938e91",
+    "url": "/sport-friend/static/js/runtime-main.b8306acc.js"
+  }
+]);

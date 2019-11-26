b3d32fa477ef96c00f0e04ac8f8f266adb6f252f
Breno Nunes <brenontavares@gmail.com>
2019-11-26 17:33:57 -0300

    img.js // sportTypes data
    creating file to export imgs and importing to sportType

--- src/components/ActivitySelection/ActivitySelection.js
+++ src/components/ActivitySelection/ActivitySelection.js
@@ -1,27 +1,15 @@
 import React from 'react'
 import './ActivitySelection.scss'
-import { categoryTypes } from './Activityjson/data.js'
+import { sportTypes } from '../../data/sportTypes'
 
-// import Football from '../../assets/img/football.png'
-// import Frame from '../../assets/img/Frame.png'
-// import gym from '../../assets/img/gym.png'
-// import running from '../../assets/img/running.png'
-// import Skate from '../../assets/img/Skate.png'
-// import walk from '../../assets/img/walk.png'
 export default function ActivitySelection() {
     return (
         <div className="main">
             <div className="container-activity">
-                {categoryTypes.map(categoryType => {
+                {sportTypes.map(type => {
                     return (<div className="activity">
                         <div className="sports">
-                            {/* <img src={categoryType.imagem} alt=''></img> */}
-                            {/* <img src={require('../../assets/img/football.png')} /> */}
-                            <img src={require({ categoryType.imagem })} />
-                            {/* <img src={require('../../assets/img/gym.png')} />
-                            <img src={require('../../assets/img/running.png')} />
-                            <img src={require('../../assets/img/Skate.png')} />
-                            <img src={require('../../assets/img/walk.png')} /> */}
+                            <img src={type.imagem} alt={type.name}/>
                         </div>
                     </div>
                     )
---
+++ src/data/sportTypes.js
@@ -0,0 +1,34 @@
+import Imagens from "../assets/img";
+
+export const sportTypes = [
+  {
+    id: 1,
+    name: "Futebol",
+    imagem: Imagens.football
+  },
+  {
+    id: 2,
+    name: "Futebol",
+    imagem: Imagens.football
+  },
+  {
+    id: 3,
+    name: "Futebol",
+    imagem: Imagens.football
+  },
+  {
+    id: 4,
+    name: "Futebol",
+    imagem: Imagens.football
+  },
+  {
+    id: 5,
+    name: "Futebol",
+    imagem: Imagens.football
+  },
+  {
+    id: 6,
+    name: "Futebol",
+    imagem: Imagens.football
+  }
+];

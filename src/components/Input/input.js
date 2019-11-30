fb6c1bd56457effdef6b1c223f8708fa5944e9b7
Breno Nunes <brenontavares@gmail.com>
2019-11-30 12:28:16 -0300

    [Gustavo] Input Component

--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import './input.scss'
+import React from "react";
+import "./input.scss";
+
 export default function Input(props) {
   return (
     <>
-    <input className="input" type={props.type} placeholder='User' />
+      {/* <i className="material-icons">lok</i>
+      <i className="material-icons">person</i> */}
+
+      <input
+        className={props.className ? props.className + " input" : "input"}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+        id={props.id}
+        name={props.name}
+        autoComplete={props.autoComplete}
+        autoFocus={props.autoFocus}
+      />
+      {/* <input placeholder={Email} /> */}
     </>
   );
 }

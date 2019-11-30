44c95141ef50ff73679fb32f8c97cc5d5d75369c
Alan Araujo <alan.techsafe@hotmail.com>
2019-11-30 12:23:46 -0300

    (HUGO AQUI) home web
    fazendo

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Ring } from "react-awesome-spinners";
-import './loading.scss';
 const Loading = () => {
     const [loading, setLoading] = useState(true)
       

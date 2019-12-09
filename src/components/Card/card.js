9a41c6504aeaa36afc430beaa902da346faadb12
Jaja <jarilson.rk@gmail.com>
2019-12-08 22:30:00 -0300

    update

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -1,66 +1,68 @@
-import React, { useState } from 'react';
-import './card.scss';
+import React, { useState } from "react";
+import "./card.scss";
 // import activities from '../../data/activities.js'
-import users from '../../data/users.js'
-import Map from '../../assets/img/maps.png'
+import users from "../../data/users.js";
+import Map from "../../assets/img/maps.png";
 export default function Card(props) {
+  // const activity = activities[0];
+  const activity = props.activity;
+  const user = users[0];
+  // const [user, setUser] = useState("");
+  const date = activity.date;
 
+  //    function getUser () {
+  //         const currentUserId = activity.author_id
+  //         const author = users.find(user => user.id === currentUserId)
+  //         setUser(author.first_name);
+  //     }
 
-    // const activity = activities[0];
-    const activity = props.activity;
-    const user = users[0];
-    // const [user, setUser] = useState("");
-    const date = activity.date;
+  const [match, setMatch] = useState(false);
 
+  //    function getUser () {
+  //         const currentUserId = activity.author_id
+  //         const author = users.find(user => user.id === currentUserId)
+  //         setUser(author.first_name);
+  //     }
 
-    //    function getUser () {
-    //         const currentUserId = activity.author_id
-    //         const author = users.find(user => user.id === currentUserId)
-    //         setUser(author.first_name);
-    //     }
+  function toogleMatch() {
+    setMatch(!match);
+    alert("foi");
+  }
 
-    const [match, setMatch] = useState(false)
-
-
-    //    function getUser () {
-    //         const currentUserId = activity.author_id
-    //         const author = users.find(user => user.id === currentUserId)
-    //         setUser(author.first_name);
-    //     }
-    
-    function toogleMatch() {
-        setMatch(!match)
-        alert("foi")
-    }
-
-    return (
-        <section className="Container">
-            <div className="box">
-                <img className="img" src={user.avatar} alt={user.first_name} />
-                <p className="User_Name">{user.first_name}</p>
-            </div>
-            <div className="Time">
-                <div className="bar">
-                    <p className="start"><i className="material-icons icon_start">alarm</i>{activity.moment.start_hour}</p>
-                    <p className="start"><i className="material-icons icon_start">alarm</i>{activity.moment.finish_hour}</p>
-                </div>
-            </div>
-            <div className="Box_info">
-                <div className="cheked">
-                    <i className="material-icons icon_start" onClick={toogleMatch}></i>
-                </div>
-                <div className="stats">
-                    <p className="User_Name">{activity.type}</p>
-                    <p className="Local">{activity.place}</p>
-                    <div className="lacalization">
-                        <p className="data">{date["day"]}/{date["month"]}/{date["year"]}</p>
-                        <img src={Map} alt=""></img>
-                    </div>
-                    {/* <p className="data">{date["day"]}/{date["month"]}/{date["year"]}</p> */}
-                </div>
-            </div>
-        </section>
-
-    );
+  return (
+    <section className="Container" onClick={props.onClick}>
+      <div className="box">
+        <img className="img" src={user.avatar} alt={user.first_name} />
+        <p className="User_Name">{user.first_name}</p>
+      </div>
+      <div className="Time">
+        <div className="bar">
+          <p className="start">
+            <i className="material-icons icon_start">alarm</i>
+            {activity.moment.start_hour}
+          </p>
+          <p className="start">
+            <i className="material-icons icon_start">alarm</i>
+            {activity.moment.finish_hour}
+          </p>
+        </div>
+      </div>
+      <div className="Box_info">
+        <div className="cheked">
+          <i className="material-icons icon_start" onClick={toogleMatch}></i>
+        </div>
+        <div className="stats">
+          <p className="User_Name">{activity.type}</p>
+          <p className="Local">{activity.place}</p>
+          <div className="lacalization">
+            <p className="data">
+              {date["day"]}/{date["month"]}/{date["year"]}
+            </p>
+            <img src={Map} alt="mapa"></img>
+          </div>
+          {/* <p className="data">{date["day"]}/{date["month"]}/{date["year"]}</p> */}
+        </div>
+      </div>
+    </section>
+  );
 }
-

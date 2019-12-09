0a45e187c221a661c31f2fee87f874478f43d750
Jaja <jarilson.rk@gmail.com>
2019-12-08 22:08:48 -0300

    implementing component modalActivity.js

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,91 +1,73 @@
-// import React from 'react';
-// // import { Link } from "react-router-dom";
-// import Header from '../../components/Header/header.js'
-// import Card from '../../components/Card/card'
-// import './home.scss';
-// import firebase from '../../services/firebase'
-// // import ActivitySelection from '../../components/ActivitySelection/ActivitySelection.js';
-// import ActivitySelection from '../../components/ActivitySelection/ActivitySelectionClass.js';
-// import activities from '../../data/activities.js';
-
-
-// export default function Home(props) {
-
-//   async function filter(params) {
-
-//   }
-
-//   if(!firebase.getCurrentUsername()) {
-//     // not logged in
-//     alert('Please login first')
-//     props.history.replace('/login')
-//     return null
-//   }
-
-//   return (
-//     <main className="flex-center">
-//         <Header />
-//         <h1 className="apresentation-title">
-//           {/* Olá {firebase.getCurrentUsername()}! <br></br>  */}
-//           Que tal encontrar uma atividade ?</h1>
-//         <ActivitySelection filter={filter} />
-//         {activities.map(activity => {
-//           return <Card activity={activity} />
-//         })}
-//     </main>
-//   );
-// }
-
-
-import React, { Component } from 'react';
-import Header from '../../components/Header/header.js'
-import Card from '../../components/Card/card'
+import React, { Component } from "react";
+import Header from "../../components/Header/header.js";
+import Card from "../../components/Card/card";
+import ModalActivity from "../../components/ModalActivity/ModalActivity";
 // import Map from '../../components/Maps/maps.js'
 // import Modal from '../../components/Modal/modal.js'
-import './home.scss';
+import "./home.scss";
 // import firebase from '../../services/firebase'
 // import ActivitySelection from '../../components/ActivitySelection/ActivitySelection.js';
-import ActivitySelection from '../../components/ActivitySelection/ActivitySelectionClass.js';
-import activities from '../../data/activities.js';
+import ActivitySelection from "../../components/ActivitySelection/ActivitySelectionClass.js";
+import activities from "../../data/activities.js";
 
 export default class Home extends Component {
-
   allActivities = activities;
 
   state = {
     filterBy: "",
     filteredActitivies: activities,
     showModal: false,
-  }
+    cardClicked: ""
+  };
 
-
-  filter = async (filterBy) => {
+  filter = async filterBy => {
     await this.setState({ filterBy: filterBy });
-    let updatedActivities = this.allActivities.filter(item => item.type === this.state.filterBy);
-    await this.setState({ filteredActitivies: updatedActivities })
-    console.log(this.state.filterBy)
-    console.log(this.state.filteredActitivies)
-  }
+    let updatedActivities = this.allActivities.filter(
+      item => item.type === this.state.filterBy
+    );
+    await this.setState({ filteredActitivies: updatedActivities });
+    console.log(this.state.filterBy);
+    console.log(this.state.filteredActitivies);
+  };
+
   toggleModal = () => {
     this.setState({
-      showModal: !this.state.showModal,
-    })
-  }
-  render() {
+      showModal: !this.state.showModal
+    });
+  };
 
+  setCardClicked = async i => {
+    await this.setState({
+      cardClicked: this.state.filteredActitivies[i]
+    });
+    console.log(this.state.cardClicked);
+    this.toggleModal();
+  };
+
+  render() {
     return (
       <main className="flex-center">
         <Header />
-        {/* <h1>Google Maps</h1>
-        <Map /> */}
-
         <h1 className="apresentation-title">
           {/* Olá {firebase.getCurrentUsername()}! <br></br>  */}
-          Que tal encontrar uma atividade ?</h1>
+          Que tal encontrar uma atividade ?
+        </h1>
         <ActivitySelection filter={this.filter} />
-        {this.state.filteredActitivies.map(activity => {
-          return <Card activity={activity} />
+        {this.state.filteredActitivies.map((activity, index) => {
+          return (
+            <Card
+              onClick={() => this.setCardClicked(index)}
+              key={index}
+              activity={activity}
+            />
+          );
         })}
+        {this.state.showModal ? (
+          <ModalActivity
+            card={this.state.cardClicked}
+            close={this.toggleModal}
+          />
+        ) : null}
       </main>
     );
   }

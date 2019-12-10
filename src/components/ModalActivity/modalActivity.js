12897c8ef96a729e38a5b927520ba0addad6f84f
Jaja <jarilson.rk@gmail.com>
2019-12-10 02:53:14 -0300

    update modal desktop

--- src/components/ModalActivity/modalActivity.js
+++ src/components/ModalActivity/modalActivity.js
@@ -13,32 +13,35 @@ export default function ModalActivity(props) {
         <i onClick={props.close} className="material-icons closeModal">
           close
         </i>
-        <section>
-          <div className="user">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_Mstcwa_KvZBi8-zxjR9NwVRD-fIye-_7bxRq19F9hylyqJcT"
-              alt="Alan"
-            />
-            <h2>Alan</h2>
+        <div className="testeBox">
+          <section>
+            <div className="user">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_Mstcwa_KvZBi8-zxjR9NwVRD-fIye-_7bxRq19F9hylyqJcT"
+                alt="Alan"
+              />
+              <h2>Alan</h2>
+            </div>
+            <div className="userPerfil">
+              <img src={Clock} alt=""></img>
+              <img src={Calendario} alt=""></img>
+              <img src={Local} alt=""></img>
+            </div>
+            <div className="Texts">
+              <p>
+                {props.card.moment.start_hour} - {props.card.moment.finish_hour}
+              </p>
+              <p>
+                {props.card.date.day}/{props.card.date.month}/
+                {props.card.date.year}
+              </p>
+              <p>{props.card.place}</p>
+            </div>
+          </section>
+          <div className="description">
+            <h2>{props.card.description}</h2>
           </div>
-          <div className="userPerfil">
-            <img src={Clock} alt=""></img>
-            <img src={Calendario} alt=""></img>
-            <img src={Local} alt=""></img>
-          </div>
-          <div className="Texts">
-            <p>
-              {props.card.moment.start_hour} - {props.card.moment.finish_hour}
-            </p>
-            <p>
-              {props.card.date.day}/{props.card.date.month}/
-              {props.card.date.year}
-            </p>
-            <p>{props.card.place}</p>
-          </div>
-        </section>
-        <div className="description">
-          <h2>{props.card.description}</h2>
+          <Button className="btnTeste " value="Confirmar Atividade" />
         </div>
         <Map />
         <Button className="btnConfirmar " value="Confirmar Atividade" />

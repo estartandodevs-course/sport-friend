0982049448004ae61399b5c34b6d93627b47c1d4
Jaja <jarilson.rk@gmail.com>
2019-12-11 01:57:23 -0300

    Fix / update

--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -18,21 +18,26 @@ export default function Input(props) {
       /> */}
 
       <div className="label-float">
-        <input 
+        <input
           // className={props.className ? props.className + " input" : "input"}
-          id={props.id} 
-          name={props.name} 
-          type={props.type} 
-          placeholder=" " 
-          onChange={props.onChange} 
+          id={props.id}
+          name={props.name}
+          type={props.type}
+          placeholder=" "
+          onChange={props.onChange}
           autoComplete={props.autoComplete}
           autoFocus={props.autoFocus}
           style={props.style}
-          />
+          className={props.className}
+          imagem={props.imagem}
+        />
         <label>{props.placeholder}</label>
-        {props.icon ? <i onClick={props.onClickIcon} className="material-icons">{props.icon}</i> : null}
+        {props.icon ? (
+          <i onClick={props.onClickIcon} className="material-icons">
+            {props.icon}
+          </i>
+        ) : null}
       </div>
-
     </>
   );
 }

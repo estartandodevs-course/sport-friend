0ad2f1dd1eb3e824545b75bf70564762d7e0fba2
Jaja <jarilson.rk@gmail.com>
2019-12-11 01:49:59 -0300

    update imagem

--- src/assets/img.js
+++ src/assets/img.js
@@ -1,24 +1,45 @@
-import football from '../assets/img/football.png'
-import frame from '../assets/img/Frame.png'
-import gym from '../assets/img/gym.png'
-import running from '../assets/img/running.png'
-import skate from '../assets/img/Skate.png'
-import walk from '../assets/img/walk.png'
-import calendar from '../assets/img/cal-card.png'
-import location from '../assets/img/loc-card.png'
-import clock from '../assets/img/clock-card.png'
-import arrows from '../assets/img/open-card.png'
+// import football from "../assets/img/football.png";
+import football from "../assets/img/football.svg";
+import frame from "../assets/img/bicicleta.svg";
+import gym from "../assets/img/exercicio.svg";
+import running from "../assets/img/corrida.svg";
+import skate from "../assets/img/skate.svg";
+import walk from "../assets/img/caminhar.svg";
+import calendar from "../assets/img/Calendario.svg";
+import location from "../assets/img/Local.svg";
+import clock from "../assets/img/clock-card.svg";
+import arrows from "../assets/img/open-card.png";
 
 export default {
-    football: football,
-    frame: frame,
-    gym: gym,
-    running: running,
-    skate: skate,
-    walk: walk,
-    calendar: calendar,
-    location: location,
-    clock: clock,
-    arrows: arrows,
+  football: football,
+  frame: frame,
+  gym: gym,
+  running: running,
+  skate: skate,
+  walk: walk,
+  calendar: calendar,
+  location: location,
+  clock: clock,
+  arrows: arrows
+};
+// import football from "../assets/img/football.svg";
+// import exercicio from "../assets/img/exercicio.svg";
+// import corrida from "../assets/img/corrida.svg";
+// import walk from "../assets/img/caminhar.svg";
+// import calendar from "../assets/img/Calendario.svg";
+// import location from "../assets/img/Local.svg";
+// import clock from "../assets/img/clock-card.png";
+// import arrows from "../assets/img/open-card.png";
 
-}
+// export default {
+//   football: football,
+//   bicicleta: bicicleta,
+//   exercicio: exercicio,
+//   corrida: corrida,
+//   skate: skate,
+//   walk: walk,
+//   calendar: calendar,
+//   location: location,
+//   clock: clock,
+//   arrows: arrows
+// };

cea754278b97986e4abce6ac0a0781820af5373c
Jaja <jarilson.rk@gmail.com>
2019-12-16 17:09:50 -0300

    update Imgs

--- src/assets/img.js
+++ src/assets/img.js
@@ -1,4 +1,3 @@
-// import football from "../assets/img/football.png";
 import football from "../assets/img/football.svg";
 import frame from "../assets/img/bicicleta.svg";
 import gym from "../assets/img/exercicio.svg";
@@ -10,15 +9,34 @@ import location from "../assets/img/Local.svg";
 import clock from "../assets/img/clock-card.svg";
 import arrows from "../assets/img/open-card.png";
 
+// activeImg //
+import footballActive from "../assets/img/activeImg/football.svg";
+import frameActive from "../assets/img/activeImg/bicicleta.svg";
+import gymActive from "../assets/img/activeImg/exercicio.svg";
+import runningActive from "../assets/img/activeImg/corrida.svg";
+import skateActive from "../assets/img/activeImg/skate.svg";
+import walkActive from "../assets/img/activeImg/caminhar.svg";
+// activeImg //
+
 export default {
+  calendar: calendar,
+  location: location,
+  clock: clock,
+  arrows: arrows,
+
+  //sports
   football: football,
   frame: frame,
   gym: gym,
   running: running,
   skate: skate,
   walk: walk,
-  calendar: calendar,
-  location: location,
-  clock: clock,
-  arrows: arrows
+  // activeImg //
+  footballActive: footballActive,
+  frameActive: frameActive,
+  gymActive: gymActive,
+  runningActive: runningActive,
+  skateActive: skateActive,
+  walkActive: walkActive
+  // activeImg //
 };

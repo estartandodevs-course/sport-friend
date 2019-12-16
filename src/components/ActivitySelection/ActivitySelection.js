06f479c470aca01db5c6f92039a9b0a30fe24ec6
Jaja <jarilson.rk@gmail.com>
2019-12-16 17:11:45 -0300

    update filter / imgs

--- src/components/ActivitySelection/ActivitySelection.js
+++ src/components/ActivitySelection/ActivitySelection.js
@@ -5,46 +5,84 @@ import { useMedia } from "../../hooks/useMedia";
 
 export default function(props) {
   const [allSports, setAllSports] = useState(sportTypes);
-
+  const [selectedSport, setSelectedSport] = useState({});
   var { isSmall } = useMedia();
 
-  const handleSelectedSport = e => {
-    const elementName = e.target.name;
-    const sports = allSports;
-    const nextSport = sports.find(obj => obj.name === elementName);
-    const nextSportIndex = sports.indexOf(nextSport);
-    
-    if(isSmall) {
-      const currentSelected = sports[4];
-      sports[nextSportIndex] = currentSelected;
-      sports[4] = nextSport;
-    }
+  // const handleSelectedSport = e => {
+  //   const elementName = e.target.name;
+  //   const sports = allSports;
+  //   const nextSport = sports.find(obj => obj.name === elementName);
+  //   const nextSportIndex = sports.indexOf(nextSport);
 
-    setAllSports(sports);
+  //   if (isSmall) {
+  //     const currentSelected = sports[4];
+  //     sports[nextSportIndex] = currentSelected;
+  //     sports[4] = nextSport;
+  //   }
+
+  //   setAllSports(sports);
+  // };
+
+  const handleSelectedSport = sport => {
+    setSelectedSport(sport);
   };
 
+  const isSelected = sport => {
+    // console.log(selectedSport.id === sport.id);
+    return selectedSport.id === sport.id;
+  };
 
   return (
     <div className="container-activity">
       {allSports.map(sport => {
+        if (isSelected(sport)) return;
         return (
           <label forhtml={sport.name} key={sport.id} className="sport">
-            <input className="input-radio" name="sportsType" id={sport.name} type="radio" />
-            <img 
-              src={sport.imagem}
+            <input
+              className="input-radio"
+              name="sportsType"
+              id={sport.name}
+              type="radio"
+            />
+            <img
+              src={isSelected(sport) ? sport.activeImg : sport.imagem}
               alt={sport.name}
               name={sport.name}
-              className="sport-img"
+              className={`sport-img ${isSelected(sport) ? "active" : ""}`}
               key={sport.id}
-              onClick={e => {
-                handleSelectedSport(e);
-                props.filter(e.target.name);
-              }}
+              onClick={() => handleSelectedSport(sport)}
             />
             <p>{sport.name}</p>
           </label>
         );
       })}
+      {!!selectedSport.id && (
+        <label
+          forhtml={selectedSport.name}
+          key={selectedSport.id}
+          className="sport"
+        >
+          <input
+            className="input-radio"
+            name="sportsType"
+            id={selectedSport.name}
+            type="radio"
+          />
+          <img
+            src={
+              isSelected(selectedSport)
+                ? selectedSport.activeImg
+                : selectedSport.imagem
+            }
+            alt={selectedSport.name}
+            name={selectedSport.name}
+            className={`sport-img ${isSelected(selectedSport) ? "active" : ""}`}
+            key={selectedSport.id}
+            onClick={() => handleSelectedSport(selectedSport)}
+          />
+          <p>{selectedSport.name}</p>
+        </label>
+      )}
     </div>
   );
 }
--- src/data/sportTypes.js
+++ src/data/sportTypes.js
@@ -4,32 +4,38 @@ export const sportTypes = [
   {
     id: 1,
     name: "Futebol",
-    imagem: Imagens.football
+    imagem: Imagens.football,
+    activeImg: Imagens.footballActive
   },
   {
     id: 2,
     name: "Exercícios",
-    imagem: Imagens.gym
+    imagem: Imagens.gym,
+    activeImg: Imagens.gymActive
   },
   {
     id: 3,
     name: "Skate",
-    imagem: Imagens.skate
+    imagem: Imagens.skate,
+    activeImg: Imagens.skateActive
   },
   {
     id: 4,
     name: "Bicicleta",
-    imagem: Imagens.frame
+    imagem: Imagens.frame,
+    activeImg: Imagens.frameActive
   },
   {
     id: 5,
     name: "Corrida",
-    imagem: Imagens.running
+    imagem: Imagens.running,
+    activeImg: Imagens.runningActive
   },
   {
     id: 6,
     name: "Caminhar",
-    imagem: Imagens.walk
+    imagem: Imagens.walk,
+    activeImg: Imagens.walkActive
   }
 ];
 export default sportTypes;

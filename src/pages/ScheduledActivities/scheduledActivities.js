b7fc16a6ba2f3d6c5f39bbaca00f676a8de9624e
Breno Nunes <brenontavares@gmail.com>
2019-12-18 21:28:45 -0300

    scheduleds activities + logo update

--- src/pages/ScheduledActivities/scheduledActivities.js
+++ src/pages/ScheduledActivities/scheduledActivities.js
@@ -1,30 +1,85 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../../components/Header/header";
-
 import "./scheduledActivities.scss";
-// import { Container } from './styles';
+import Firebase from "../../services/firebase"
+import Service from "../../services/index"
+import Card from "../../components/Card/card";
 
 export default function ScheduledActivities() {
+  const service = new Service();
+  const initialState = {
+    allActivities: [],
+    scheduledActivities: [],
+    currentUserUid: ""
+  };
+  const [state, setState] = useState(initialState)
+
+  const {
+    allActivities,
+    scheduledActivities,
+    currentUserUid
+  } = state
+
+  useEffect(() => {
+    const uid = Firebase.getCurrentUserUid();
+    service.getActivities()
+    service.Activities.subscribe(_activities => {
+      setState({
+        ...state,
+        allActivities: _activities,
+        currentUserUid: uid
+      })
+    })
+    handleScheduled()
+  }, [allActivities])
+
+  const handleScheduled = () => {
+    allActivities.forEach(activity => {
+      console.log(activity)
+
+      if(activity.matches) {
+        //pegando cada match do array
+        activity.matches.forEach(match => {
+          if(match.uid == currentUserUid){
+            const scheduleds = scheduledActivities;
+            scheduleds.push(activity);
+
+            setState({
+              ...state,
+              scheduledActivities: scheduleds
+            })
+          }
+        })
+      }
+    })
+  }
+
   return (
     <section className="containerAtividades space-menu">
       <Header className="borderBotton" />
-      <div className="alertAtividade">
+      {/* <div className="alertAtividade">
         <h2>
           Para adicionar uma atividade <br /> clique no botão abaixo.
         </h2>
-      </div>
+      </div> */}
+      {
+        scheduledActivities.map((activity, index) => {
+            return (
+                <Card
+                  key={index}
+                  activity={activity}
+                  onClick={() => console.log("clicked")}
+                />
+              );
+          })
+      }
       <div className="user">
         <Link to="/">
           <i className="material-icons addCircle">add</i>
         </Link>
         <h1>Escolher atividade</h1>
       </div>
-      <Link
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        rel="stylesheet"
-      ></Link>
     </section>
   );
 }

4bee5c8d544d6d359cdb67070397166be0c38415
Alan Araujo <alan.techsafe@hotmail.com>
2019-12-20 01:53:19 -0300

    Fix my schedules view

--- src/pages/ScheduledActivities/scheduledActivities.js
+++ src/pages/ScheduledActivities/scheduledActivities.js
@@ -2,60 +2,46 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../../components/Header/header";
 import "./scheduledActivities.scss";
-import Firebase from "../../services/firebase"
-import Service from "../../services/index"
+import Firebase from "../../services/firebase";
+import Service from "../../services/index";
 import Card from "../../components/Card/card";
 
 export default function ScheduledActivities() {
   const service = new Service();
   const initialState = {
-    allActivities: [],
-    scheduledActivities: [],
-    currentUserUid: ""
+    scheduledActivities: []
   };
-  const [state, setState] = useState(initialState)
+  const [state, setState] = useState(initialState);
 
-  const {
-    allActivities,
-    scheduledActivities,
-    currentUserUid
-  } = state
+  const { scheduledActivities } = state;
 
   useEffect(() => {
     const uid = Firebase.getCurrentUserUid();
-    service.getActivities()
-    service.Activities.subscribe(_activities => {
+    service.getActivities();
+    service.Activities.subscribe(allActivities => {
       setState({
         ...state,
-        allActivities: _activities,
-        currentUserUid: uid
-      })
-    })
-    handleScheduled()
-  }, [allActivities])
+        scheduledActivities: handleScheduled(allActivities, uid)
+      });
+    });
+  }, []);
 
-  const handleScheduled = () => {
-    const scheduleds = scheduledActivities;
+  const handleScheduled = (_activities, _uid) => {
+    const scheduleds = [];
 
-    allActivities.forEach(activity => {
-      console.log(activity)
-
-      if(activity.matches) {
+    _activities.forEach(activity => {
+      if (activity.matches) {
         //getting each match of array
         activity.matches.forEach(match => {
-          if(match.uid === currentUserUid){
+          if (match.uid === _uid) {
             scheduleds.push(activity);
           }
-        })
+        });
       }
-    })
+    });
 
-    setState({
-      ...state,
-      scheduledActivities: scheduleds
-    })
-    
-  }
+    return scheduleds;
+  };
 
   return (
     <section className="containerAtividades space-menu">
@@ -65,17 +51,15 @@ export default function ScheduledActivities() {
           Para adicionar uma atividade <br /> clique no botão abaixo.
         </h2>
       </div> */}
-      {
-        scheduledActivities.map((activity, index) => {
-            return (
-                <Card
-                  key={index}
-                  activity={activity}
-                  onClick={() => console.log("clicked")}
-                />
-              );
-          })
-      }
+      {scheduledActivities.map((activity, index) => {
+        return (
+          <Card
+            key={index}
+            activity={activity}
+            onClick={() => console.log("clicked")}
+          />
+        );
+      })}
       <div className="add-activity">
         <Link to="/">
           <i className="material-icons addCircle">add</i>
